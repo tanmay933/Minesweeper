dac923eceaafee543828107bf472c0128f34a53c
tanmay933 <tanmaymittal965@gmail.com>
2025-10-24 16:16:14 +0530

    frontend deploy build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    host:"0.0.0.0",
+    port: Number(process.env.PORT) || 5173,
     proxy: {
       "/api": {
         target: "http://localhost:5001", // ✅ fixed port
