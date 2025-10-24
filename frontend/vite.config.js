51a17468d6568c1d6ff771564a01e40610989e88
tanmay933 <tanmaymittal965@gmail.com>
2025-10-24 16:19:18 +0530

    frontend deploy build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,22 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host:"0.0.0.0",
+    host: "0.0.0.0",
     port: Number(process.env.PORT) || 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:5001", // ✅ fixed port
+        target: "http://localhost:5001",
         changeOrigin: true,
         secure: false,
       },
     },
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: Number(process.env.PORT) || 4173,
+    allowedHosts: [
+      "minesweeper-frontend-lto2.onrender.com", // Add your Render domain here
+      "localhost",
+    ],
   },
 });
